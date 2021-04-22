6428f6a9bc19abcd01b031426ea829c7732ec54b
Jeff Bond <jeffreybond@mac.com>
2021-04-22 13:01:49 -0400

    Add Location View controller again to try to debug errors

---
+++ BLAK/AddLocationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddLocationViewController.swift
+//  BLAK
+//
+//  Created by Jeff Bond on 4/22/21.
+//
+
+import UIKit
+import MapKit
+
+class AddLocationViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
